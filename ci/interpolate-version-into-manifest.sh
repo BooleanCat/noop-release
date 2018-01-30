171a8c04094870cd9577a3f1381254a6de23ee67
Craig Furman <cfurman@pivotal.io>
2018-01-30 15:28:24 +0000

    Use correct path to git repo to get version
    Signed-off-by: Tom Godkin <tgodkin@pivotal.io>

--- ci/interpolate-version-into-manifest.sh
+++ ci/interpolate-version-into-manifest.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+cd noop-release
+
 version="0+dev.$(git rev-parse --short HEAD)"
 opsfile_contents="[{\"type\": \"replace\", \"path\": \"/releases/name=noop/version\", \"value\": \"$version\"}]"
 bosh int -o <(echo "$opsfile_contents") manifests/noop.yml > ../manifest/manifest.yml
