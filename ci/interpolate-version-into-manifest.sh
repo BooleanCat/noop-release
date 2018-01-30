76f752c471f28f3f31961038017f58bf2e47e714
Tom Godkin <tgodkin@pivotal.io>
2018-01-30 16:22:02 +0000

    first final release
    Signed-off-by: Craig Furman <cfurman@pivotal.io>

--- ci/interpolate-version-into-manifest.sh
+++ ci/interpolate-version-into-manifest.sh
@@ -6,6 +6,3 @@ cd noop-release
 version="0+dev.$(git rev-parse --short HEAD)"
 opsfile_contents="[{\"type\": \"replace\", \"path\": \"/releases/name=noop/version\", \"value\": \"$version\"}]"
 bosh int -o <(echo "$opsfile_contents") manifests/noop.yml > ../manifest/manifest.yml
-
-# TODO remove
-cat ../manifest/manifest.yml
